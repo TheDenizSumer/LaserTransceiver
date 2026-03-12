480d191ab8b4c90f0d72c56617397facbf08d80b
Liam Livingston <liamklivingston@gmail.com>
2026-03-12 16:46:54 -0700

    update to align.py

--- demoreal/align.py
+++ demoreal/align.py
@@ -1,5 +1,11 @@
 import pigpio
 import sys
+import argparse
+
+# Set up the argument parser
+parser = argparse.ArgumentParser(description="Control GPIO 27 using pigpio.")
+parser.add_argument('--stop', action='store_true', help="Turn GPIO 27 OFF instead of ON")
+args = parser.parse_args()
 
 # Define the GPIO pin (Broadcom/BCM numbering)
 PIN = 27
@@ -16,15 +22,18 @@ try:
     # Set the pin mode to OUTPUT
     pi.set_mode(PIN, pigpio.OUTPUT)
 
-    # Write a 1 (HIGH) to turn the GPIO pin ON
-    pi.write(PIN, 1)
-    
-    print(f"GPIO {PIN} is now ON.")
+    if args.stop:
+        # Write a 0 (LOW) to turn the GPIO pin OFF
+        pi.write(PIN, 0)
+        print(f"GPIO {PIN} is now OFF.")
+    else:
+        # Write a 1 (HIGH) to turn the GPIO pin ON
+        pi.write(PIN, 1)
+        print(f"GPIO {PIN} is now ON.")
 
 except Exception as e:
     print(f"An error occurred: {e}")
 
 finally:
     # Close the connection to the daemon
-    # Note: The pin will remain ON even after the script exits
     pi.stop()
