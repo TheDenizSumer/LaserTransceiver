81a52355be62e21d0991fd2e41c33e2925827d2b
Test Account <denizesumer@gmail.com>
2026-03-11 22:25:37 -0700

    testing waveForm5

--- test.py
+++ test.py
@@ -2,44 +2,38 @@ from outQueue import transmitPacket
 import asyncio
 
 packet_data = 0b0100100110110 # Example packet data
-
 import pigpio
 
 pi = pigpio.pi()
-import time
+GPIO_PIN = 17
+BIT_RATE = 1000  # Bits per second
+HALF_BIT_TIME = int(1000000 / BIT_RATE / 2) # Microseconds
 
-TX_PIN = 27
-bit_time = 1000 #mircoseconds
-BIT_PERIOD = bit_time * 1/1000000 # convert to seconds
-
-pi.set_mode(TX_PIN, pigpio.OUTPUT)
-
-
-
-def send_packet(bits):
-    for b in bits:
-        pi = pigpio.pi()
-        GPIO = TX_PIN
-
-        # 1. Define pulses (on_mask, off_mask, delay)
-        pulses = []
-
-        if b == 0:
-            pulses.append(pigpio.pulse(1<<GPIO, 0, int(bit_time/2))) # Pin 17 ON for half bit time
-            pulses.append(pigpio.pulse(0, 1<<GPIO, int(bit_time/2))) # Pin 17 OFF for half bit time
+def create_manchester_wave(data_byte):
+    pulses = []
+    
+    # Iterate through each bit (MSB first)
+    for i in range(7, -1, -1):
+        bit = (data_byte >> i) & 1
+        
+        if bit == 1:
+            # Bit 1: High for half, Low for half (10)
+            pulses.append(pigpio.pulse(1 << GPIO_PIN, 0, HALF_BIT_TIME))
+            pulses.append(pigpio.pulse(0, 1 << GPIO_PIN, HALF_BIT_TIME))
         else:
-            pulses.append(pigpio.pulse(0, 1<<GPIO, int(bit_time/2))) # Pin 17 OFF for half bit time
-            pulses.append(pigpio.pulse(1<<GPIO, 0, int(bit_time/2))) # Pin 17 ON for half bit time
-
-
+            # Bit 0: Low for half, High for half (01)
+            pulses.append(pigpio.pulse(0, 1 << GPIO_PIN, HALF_BIT_TIME))
+            pulses.append(pigpio.pulse(1 << GPIO_PIN, 0, HALF_BIT_TIME))
+            
     pi.wave_clear()
     pi.wave_add_generic(pulses)
-    wave_id = pi.wave_create()
+    return pi.wave_create()
 
-    pi.wave_send_repeat(wave_id) # Hardware takes over here!
+# Usage
+wave_id = create_manchester_wave(0x41) # Send ASCII 'A' (01000001)
+pi.wave_send_once(wave_id)
 
-
-packet = [0,1,0,1,1,0,0,1,0,0]
-send_packet(packet)
-
-
+# Cleanup after sending
+while pi.wave_tx_busy():
+    pass
+pi.wave_delete(wave_id)
